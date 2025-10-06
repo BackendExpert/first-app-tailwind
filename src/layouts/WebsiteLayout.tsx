ce9f743235ee52b027506275ef0c49e2edef89dc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-10-06 12:08:07 +0530

    Update WebsiteLayout.tsx

--- src/layouts/WebsiteLayout.tsx
+++ src/layouts/WebsiteLayout.tsx
@@ -6,19 +6,16 @@ import React from 'react';
 export default function WebSiteLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen bg-gradient-to-br from-rose-100 via-white to-pink-200">
-            {/* NAVBAR */}
+            
             <div className="">
                 <Navbar />
             </div>
 
-            {/* MAIN CONTENT */}
             <main className="">
                 {children}
             </main>
 
             <Footer />
         </div>
-
-
     )
 }
